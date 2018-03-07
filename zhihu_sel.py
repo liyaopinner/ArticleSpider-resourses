8a41680347f5132bdf8b2f289e091a987e966b01
liyaopinner <liyao.pinner@gmail.com>
2018-03-07 17:57:23 +0800

    Update zhihu_sel.py

--- zhihu_sel.py
+++ zhihu_sel.py
@@ -51,9 +51,9 @@ class ZhihuSpider(scrapy.Spider):
 
         browser.get("https://www.zhihu.com/signin")
         browser.find_element_by_css_selector(".SignFlow-accountInput.Input-wrapper input").send_keys(
-            "18782902568")
+            "xxxx")
         browser.find_element_by_css_selector(".SignFlow-password input").send_keys(
-            "admin321")
+            "xxxx")
         browser.find_element_by_css_selector(
             ".Button.SignFlow-submitButton").click()
         import time
